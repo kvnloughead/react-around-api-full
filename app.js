9b9a9c6a99a0f78b742642b9b8eb8b7c2888b4f9
Kevin Loughead <kvnloughead@gmail.com>
2020-09-05 10:34:21 -0400

    first-commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const users = require('./routes/users.js');
+const cards = require('./routes/cards.js');
+
+const { PORT = 3000 } = process.env;
+const app = express();
+
+app.use('/', users);
+app.use('/', cards);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(PORT, () => {
+  console.log(`App listening at port ${PORT}`);
+});
---
+++ public/precache-manifest.8a0c950eae5d1d750aa525663099c46f.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3076ecc0aa142c2afc9d0e66101dba8f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59d9460d37724dfbab5e",
+    "url": "/static/css/main.8106defd.chunk.css"
+  },
+  {
+    "revision": "b66c7475a8c5e39bc984",
+    "url": "/static/js/2.87353fb1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.87353fb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59d9460d37724dfbab5e",
+    "url": "/static/js/main.22b2a6ac.chunk.js"
+  },
+  {
+    "revision": "e45147162a4124440559",
+    "url": "/static/js/runtime-main.335667d2.js"
+  },
+  {
+    "revision": "e37354838bf8078ac0d296217613533f",
+    "url": "/static/media/Inter-Black.e3735483.woff"
+  },
+  {
+    "revision": "79260e5b693fac2ee373b659f01a2bdf",
+    "url": "/static/media/Inter-Bold.79260e5b.woff"
+  },
+  {
+    "revision": "7a8cc7241f766a142e15b2948804e547",
+    "url": "/static/media/Inter-Medium.7a8cc724.woff"
+  },
+  {
+    "revision": "7c539936c4c8c822b59a1bcc6c08a6ec",
+    "url": "/static/media/Inter-Regular.7c539936.woff"
+  },
+  {
+    "revision": "4056b612a59cf5b2722bc52744a46b58",
+    "url": "/static/media/close-icon.4056b612.svg"
+  },
+  {
+    "revision": "b9b65c7e7e563b8df5ac93cc01872a72",
+    "url": "/static/media/edit-pen.b9b65c7e.svg"
+  },
+  {
+    "revision": "1406fd9902df5ffa08a784292c6bb149",
+    "url": "/static/media/finger.1406fd99.svg"
+  },
+  {
+    "revision": "71ad3aff95174c163373361088501a54",
+    "url": "/static/media/header-logo.71ad3aff.svg"
+  },
+  {
+    "revision": "b12167400da3396be4b9b6e69ba27f5d",
+    "url": "/static/media/heart-been-clicked.b1216740.svg"
+  },
+  {
+    "revision": "e7a8906a3b0887219bad0d62de4f7612",
+    "url": "/static/media/heart.e7a8906a.svg"
+  },
+  {
+    "revision": "ba6e248c8475a38697df89031ed033f8",
+    "url": "/static/media/pen.ba6e248c.svg"
+  },
+  {
+    "revision": "50cf486163827b9b62c764268a435184",
+    "url": "/static/media/plus.50cf4861.svg"
+  },
+  {
+    "revision": "99e3c3fbcf56784848ac8f1b84394bcd",
+    "url": "/static/media/trashcan.99e3c3fb.svg"
+  },
+  {
+    "revision": "bdf7042b09c297fb1d3033147bb5aae5",
+    "url": "/static/media/yosemite.bdf7042b.jpg"
+  }
+]);
