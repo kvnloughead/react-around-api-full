844b36695f17c00122024e995bf7dfeb538b9f63
Kevin Loughead <kvnloughead@gmail.com>
2020-09-20 15:02:16 -0400

    feat: error handling improved

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,22 +1,36 @@
 const Card = require('../models/card');
 
+const errorCodes = {
+  ValidationError: 400,
+  CastError: 404,
+};
+
 module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
   Card.create({ name, link, owner: req.user._id })
     // .populate('owner')`
     .then((card) => res.send({ data: card }))
-    .catch((err) => res.status(500).send({ message: 'Error', error: err }));
+    .catch((err) => {
+      const statusCode = errorCodes[err.name] || 500;
+      res.status(statusCode).send({ message: 'Error', [err.name]: err });
+    });
 };
 
 module.exports.getCards = (req, res) => {
   Card.find({})
     // .populate('owner')
     .then((card) => res.send({ data: card }))
-    .catch((err) => res.status(500).send({ message: 'Error', error: err }));
+    .catch((err) => {
+      const statusCode = errorCodes[err.name] || 500;
+      res.status(statusCode).send({ message: 'Error', [err.name]: err });
+    });
 };
 
 module.exports.deleteCardById = (req, res) => {
   Card.findByIdAndRemove(req.params.id)
     .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: 'Error', error: err }));
+    .catch((err) => {
+      const statusCode = errorCodes[err.name] || 500;
+      res.status(statusCode).send({ message: 'Error', [err.name]: err });
+    });
 };
--- controllers/users.js
+++ controllers/users.js
@@ -1,20 +1,34 @@
 const User = require('../models/user');
 
-module.exports.createUser = (req, res) => {
-  const { name, about, avatar } = req.body;
-  User.create({ name, about, avatar })
-    .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: 'Error', error: err }));
+const errorCodes = {
+  ValidationError: 400,
+  CastError: 404,
 };
 
 module.exports.getUsers = (req, res) => {
   User.find({})
     .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: 'Error', error: err }));
+    .catch((err) => {
+      const statusCode = errorCodes[err.name] || 500;
+      res.status(statusCode).send({ message: 'Error', [err.name]: err });
+    });
 };
 
 module.exports.getUserById = (req, res) => {
   User.findById(req.params.id)
     .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: 'Error', error: err }));
+    .catch((err) => {
+      const statusCode = errorCodes[err.name] || 500;
+      res.status(statusCode).send({ message: 'Error', [err.name]: err });
+    });
+};
+
+module.exports.createUser = (req, res) => {
+  const { name, about, avatar } = req.body;
+  User.create({ name, about, avatar })
+    .then((user) => res.send({ data: user }))
+    .catch((err) => {
+      const statusCode = errorCodes[err.name] || 500;
+      res.status(statusCode).send({ message: 'Error', [err.name]: err });
+    });
 };
