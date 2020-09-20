737858b851601fb1154257693655af711929a14d
Kevin Loughead <kvnloughead@gmail.com>
2020-09-20 14:24:18 -0400

    chore: remove data and public directories

--- routes/cards.js
+++ routes/cards.js
@@ -1,24 +1,9 @@
 const router = require('express').Router();
-// const path = require('path');
-// const fs = require('fs');
 
 const { createCard, getCards, deleteCardById } = require('../controllers/cards');
 
 router.get('/', getCards);
 router.post('/', createCard);
 router.delete('/:id', deleteCardById);
-
-// router.get('/', (req, res) => {
-//   const filePath = path.join(__dirname, '..', 'data', 'cards.json');
-//   fs.readFile(filePath, { encoding: 'utf8' }, (err, data) => {
-//     if (err) {
-//       res.status(500).json({ message: 'Requested resource not found' });
-//       console.log(err);
-//       return;
-//     }
-//     const cards = JSON.parse(data);
-//     res.send(cards);
-//   });
-// });
 
 module.exports = router;
